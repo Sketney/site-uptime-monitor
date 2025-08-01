f33438c18b5ddadf7ec96eaa174a780fbfe23f65
Sketney <134638115+Sketney@users.noreply.github.com>
2025-08-01 23:36:59 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,6 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise ValueError("❌ DATABASE_URL is not set. Check your docker-compose.yml")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
