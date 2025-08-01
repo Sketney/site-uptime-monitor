b3a79f5cafe19fb1db5a82b0bfc15c9dcb20986d
Sketney <134638115+Sketney@users.noreply.github.com>
2025-08-01 22:03:14 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from .database import Base
+from datetime import datetime
+
+class Check(Base):
+    __tablename__ = "checks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    url = Column(String, nullable=False)
+    final_url = Column(String)
+    status_code = Column(Integer)
+    response_time = Column(Float)
+    checked_at = Column(DateTime, default=datetime.utcnow)
