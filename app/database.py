08781021f4b15b58a1ab19565c2c0cf68bcae6f3
Sketney <134638115+Sketney@users.noreply.github.com>
2025-08-02 13:07:25 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,10 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    print("⚠️ DATABASE_URL not set. Using SQLite for tests.")
-    DATABASE_URL = "sqlite:///:memory:"
+    print("⚠️ DATABASE_URL not set. Using local SQLite test.db")
+    DATABASE_URL = "sqlite:///./test.db"
 
-# Для SQLite нужен спец.параметр connect_args
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
